acf64abc9c48025ced60a063a7150fa13b631e30
amshawn <shawn.yong@clarity.cx>
2021-09-14 11:41:35 +0400

    remove exception

--- ECC Integration/BO_PricingConditions_OnAddUpdate.py
+++ ECC Integration/BO_PricingConditions_OnAddUpdate.py
@@ -112,146 +112,141 @@ distCh		= "10"
 divOrg		= "PG"
 #/!\# to change - END   #/!\#
 
+from Newtonsoft.Json import JsonConvert
+from Newtonsoft.Json.Linq import JObject
+from datetime import datetime
 
-try:
-	from Newtonsoft.Json import JsonConvert
-	from Newtonsoft.Json.Linq import JObject
-	from datetime import datetime
+# Define attributes to retrieve value from configuration
+# store value in variables
+mgType			= Product.Attributes.GetByName("MG_TYPE").GetValue()
 
-	# Define attributes to retrieve value from configuration
-	# store value in variables
-	mgType			= Product.Attributes.GetByName("MG_TYPE").GetValue()
+#startDate 		= Product.Attributes.GetByName("MG_VALIDITY_START_DATE").GetValue()
+#endDate		= Product.Attributes.GetByName("MG_VALIDITY_END_DATE").GetValue()
 
-	#startDate 		= Product.Attributes.GetByName("MG_VALIDITY_START_DATE").GetValue()
-	#endDate		= Product.Attributes.GetByName("MG_VALIDITY_END_DATE").GetValue()
+startDate	   = datetime.strptime(Product.Attr('MG_VALIDITY_START_DATE').GetValue(), '%m/%d/%Y').ToString().split(' ')[0].replace('-', '')
+endDate		 = datetime.strptime(Product.Attr('MG_VALIDITY_END_DATE').GetValue(), '%m/%d/%Y').ToString().split(' ')[0].replace('-', '')
 
-	startDate	   = datetime.strptime(Product.Attr('MG_VALIDITY_START_DATE').GetValue(), '%m/%d/%Y').ToString().split(' ')[0].replace('-', '')
-	endDate		 = datetime.strptime(Product.Attr('MG_VALIDITY_END_DATE').GetValue(), '%m/%d/%Y').ToString().split(' ')[0].replace('-', '')
+currency		= Product.Attributes.GetByName("MG_CURRENCY_AUTO").GetValue()
+uom			 = Product.Attributes.GetByName('MG_UOM').SelectedValue.ValueCode
 
-	currency		= Product.Attributes.GetByName("MG_CURRENCY_AUTO").GetValue()
-	uom			 = Product.Attributes.GetByName('MG_UOM').SelectedValue.ValueCode
+soldTo 			= Product.Attributes.GetByName("MG_SOLD_TO").GetValue()
+shipTo 			= Product.Attributes.GetByName("MG_SHIP_TO").GetValue()
+endCust 		= Product.Attributes.GetByName("MG_END_CUSTOMER").GetValue()
+endCustCde		= Product.Attributes.GetByName("MG_END_CUSTOMER").SelectedValue.ValueCode[4:] if Product.Attributes.GetByName("MG_END_CUSTOMER").SelectedValue else ""
+endUseObject 	= Product.Attributes.GetByName("MG_END_USE_OBJECT").GetValue()
+endObjCde		= Product.Attributes.GetByName("MG_END_USE_OBJECT").SelectedValue.ValueCode if Product.Attributes.GetByName("MG_END_USE_OBJECT").SelectedValue else ""
 
-	soldTo 			= Product.Attributes.GetByName("MG_SOLD_TO").GetValue()
-	shipTo 			= Product.Attributes.GetByName("MG_SHIP_TO").GetValue()
-	endCust 		= Product.Attributes.GetByName("MG_END_CUSTOMER").GetValue()
-	endCustCde		= Product.Attributes.GetByName("MG_END_CUSTOMER").SelectedValue.ValueCode[4:] if Product.Attributes.GetByName("MG_END_CUSTOMER").SelectedValue else ""
-	endUseObject 	= Product.Attributes.GetByName("MG_END_USE_OBJECT").GetValue()
-	endObjCde		= Product.Attributes.GetByName("MG_END_USE_OBJECT").SelectedValue.ValueCode if Product.Attributes.GetByName("MG_END_USE_OBJECT").SelectedValue else ""
+# check if attribute is empty, else assign 'X'
+sold2  			= "" if (soldTo is None or soldTo == "") else "X"
+shipTo2 		= "" if (shipTo is None or shipTo == "") else "X"
+endCust2 		= "" if (endCust is None or endCust == "") else "X"
+endUseObject2 	= "" if (endUseObject is None or endUseObject == "") else "X"
 
-	# check if attribute is empty, else assign 'X'
-	sold2  			= "" if (soldTo is None or soldTo == "") else "X"
-	shipTo2 		= "" if (shipTo is None or shipTo == "") else "X"
-	endCust2 		= "" if (endCust is None or endCust == "") else "X"
-	endUseObject2 	= "" if (endUseObject is None or endUseObject == "") else "X"
+# get access sequence from BO_PRICING_CONDITIONS, based on configuration
+sqlResult 	= SqlHelper.GetSingle("""SELECT COND_TYPE, PRIORITY, TABLE_NUM
+								FROM BO_PRICING_CONDITIONS WHERE
+								SOLD_TO = '%s' AND
+								SHIP_TO = '%s' AND
+								END_USER = '%s' AND
+								END_USE_OBJECT = '%s' """ %(sold2, shipTo2, endCust2, endUseObject2))
 
-	# get access sequence from BO_PRICING_CONDITIONS, based on configuration
-	sqlResult 	= SqlHelper.GetSingle("""SELECT COND_TYPE, PRIORITY, TABLE_NUM
-									FROM BO_PRICING_CONDITIONS WHERE
-									SOLD_TO = '%s' AND
-									SHIP_TO = '%s' AND
-									END_USER = '%s' AND
-									END_USE_OBJECT = '%s' """ %(sold2, shipTo2, endCust2, endUseObject2))
+# Store condition type, priority, table number in variables
+condType 			= sqlResult.COND_TYPE
+priority 			= sqlResult.PRIORITY
+tableNum 			= sqlResult.TABLE_NUM
 
-	# Store condition type, priority, table number in variables
-	condType 			= sqlResult.COND_TYPE
-	priority 			= sqlResult.PRIORITY
-	tableNum 			= sqlResult.TABLE_NUM
+#get fields to build the access sequence
+erpTable = getErpTable(tableNum)
 
-	#get fields to build the access sequence
-	erpTable = getErpTable(tableNum)
+# Read the value of hidden attribute "Sold-To"
+allSoldTo 			= Product.Attributes.GetByName("MG_SOLD_TO_VALUECODES").GetValue()
+# Break down the string of value into a list
+allSoldToList	   = allSoldTo[1:-1].split(", ") if allSoldTo != "['']" else list()
 
-	# Read the value of hidden attribute "Sold-To"
-	allSoldTo 			= Product.Attributes.GetByName("MG_SOLD_TO_VALUECODES").GetValue()
-	# Break down the string of value into a list
-	allSoldToList	   = allSoldTo[1:-1].split(", ") if allSoldTo != "['']" else list()
+# Read the value of hidden attribute "Ship-To"
+allShipTo 			= Product.Attributes.GetByName("MG_SHIP_TO_VALUECODES").GetValue()
+# Break down the string of value into a list
+allShipToList 		= allShipTo[1:-1].split(", ") if allShipTo != "['']" else list()
 
-	# Read the value of hidden attribute "Ship-To"
-	allShipTo 			= Product.Attributes.GetByName("MG_SHIP_TO_VALUECODES").GetValue()
-	# Break down the string of value into a list
-	allShipToList 		= allShipTo[1:-1].split(", ") if allShipTo != "['']" else list()
+# define Pricing container
+pricingContainer	= Product.GetContainerByName('BO_PRICING_CONT')
+pricingMatCodeList 	= list()
+pricingMatPrice	 = dict()
 
-	# define Pricing container
-	pricingContainer	= Product.GetContainerByName('BO_PRICING_CONT')
-	pricingMatCodeList 	= list()
-	pricingMatPrice	 = dict()
+# Loop in pricing container to get values
+for row in pricingContainer.Rows:
+	for column in row.Columns:
+		if column.Name == "INVOICED_PRICE":
+			# value  of Invoice Price column
+			invPrice = column.Value if column.Value != '' else '0.00'
+		if column.Name == "MATERIAL_CODE":
+			# add each 'Material Code' in a list
+			pricingMatCodeList.append(column.Value)
+			# build a dictionary of 'Material Code' : 'Invoice Price'
+			pricingMatPrice[column.Value] = invPrice
 
-	# Loop in pricing container to get values
-	for row in pricingContainer.Rows:
-		for column in row.Columns:
-			if column.Name == "INVOICED_PRICE":
-				# value  of Invoice Price column
-				invPrice = column.Value if column.Value != '' else '0.00'
-			if column.Name == "MATERIAL_CODE":
-				# add each 'Material Code' in a list
-				pricingMatCodeList.append(column.Value)
-				# build a dictionary of 'Material Code' : 'Invoice Price'
-				pricingMatPrice[column.Value] = invPrice
+# Define condition key list which will contain all pricing conditions
+conditionKey = list()
 
-	# Define condition key list which will contain all pricing conditions
-	conditionKey = list()
-
-	# Build the Pricing structure
-	for matCode in pricingMatCodeList:
-		# initialize variable key
-		varKey = ""
-		# get the price for material
-		rate = pricingMatPrice[matCode]
-		# check if Sold-To list is not empty
-		if allSoldToList:
-			for soldTos in allSoldToList:
-				# check if Ship-To list is not empty
-				if allShipToList:
-					for shipTos in allShipToList:
-						# build variable key
-						varKey = getVarKey(varKey,
-											erpTable,
-											soldTos[4:-1], 	#Sold-to party
-											shipTos[4:-1],	#Ship-to party
-											sOrg,			#sales organisation
-											distCh, 		#Distribution Channel
-											matCode,		#Pricing Ref. Matl
-											endCustCde,		#End user
-											endObjCde		#end use object
-											)
-						# add the pricing content to 'condition key'
-						conditionKey.append(get_price_content(tableNum, condType, varKey, sOrg, distCh, divOrg))
-						varKey = ""
-				else:
+# Build the Pricing structure
+for matCode in pricingMatCodeList:
+	# initialize variable key
+	varKey = ""
+	# get the price for material
+	rate = pricingMatPrice[matCode]
+	# check if Sold-To list is not empty
+	if allSoldToList:
+		for soldTos in allSoldToList:
+			# check if Ship-To list is not empty
+			if allShipToList:
+				for shipTos in allShipToList:
+					# build variable key
 					varKey = getVarKey(varKey,
 										erpTable,
 										soldTos[4:-1], 	#Sold-to party
-										"",				#Ship-to party
+										shipTos[4:-1],	#Ship-to party
 										sOrg,			#sales organisation
 										distCh, 		#Distribution Channel
 										matCode,		#Pricing Ref. Matl
 										endCustCde,		#End user
 										endObjCde		#end use object
 										)
-					conditionKey.append(get_price_content(tableNum, ct, varKey, sOrg, distCh, divOrg))
-					varKey = ""		# check if Ship-To list is not empty
-		else:
-			varKey = getVarKey(varKey,
-								erpTable,
-								"",				#Sold-to party
-								"",				#Ship-to party
-								sOrg,			#sales organisation
-								distCh, 		#Distribution Channel
-								matCode,		#Pricing Ref. Matl
-								endCustCde,		#End user
-								endObjCde		#end use object
-								)
-			conditionKey.append(get_price_content(tableNum, ct, varKey, sOrg, distCh, divOrg))
-			varKey = ""
+					# add the pricing content to 'condition key'
+					conditionKey.append(get_price_content(tableNum, condType, varKey, sOrg, distCh, divOrg))
+					varKey = ""
+			else:
+				varKey = getVarKey(varKey,
+									erpTable,
+									soldTos[4:-1], 	#Sold-to party
+									"",				#Ship-to party
+									sOrg,			#sales organisation
+									distCh, 		#Distribution Channel
+									matCode,		#Pricing Ref. Matl
+									endCustCde,		#End user
+									endObjCde		#end use object
+									)
+				conditionKey.append(get_price_content(tableNum, condType, varKey, sOrg, distCh, divOrg))
+				varKey = ""		# check if Ship-To list is not empty
+	else:
+		varKey = getVarKey(varKey,
+							erpTable,
+							"",				#Sold-to party
+							"",				#Ship-to party
+							sOrg,			#sales organisation
+							distCh, 		#Distribution Channel
+							matCode,		#Pricing Ref. Matl
+							endCustCde,		#End user
+							endObjCde		#end use object
+							)
+		conditionKey.append(get_price_content(tableNum, condType, varKey, sOrg, distCh, divOrg))
+		varKey = ""
 
-	# build pricing data
-	pricingData	 = price_cond(conditionKey)
-	# serialize the data
-	priceDataJson   = RestClient.SerializeToJson(pricingData)
-	# Compress the xml data
-	import zlib
-	compressedPayload = zlib.compress(priceDataJson)
-	# assign the data to hidden attribute - BO_PRICE_ECC
-	Product.Attr('BO_PRICE_ECC').AssignValue(compressedPayload)
-
-except Exception as e:
-	Trace.Write("Error in script BO_PricingConditions_OnAddUpdate--> "+str(e))
+# build pricing data
+pricingData	 = price_cond(conditionKey)
+# serialize the data
+priceDataJson   = RestClient.SerializeToJson(pricingData)
+# Compress the xml data
+import zlib
+compressedPayload = zlib.compress(priceDataJson)
+# assign the data to hidden attribute - BO_PRICE_ECC
+Product.Attr('BO_PRICE_ECC').AssignValue(compressedPayload)
